2577237c31aec0058499f29d74673a21dd3c5665
dtaata <ahmet.ata@ext.digiturk.com.tr>
2019-10-14 16:51:05 +0300

    update base view model

--- src/main/java/com/github/app/ui/main/home/HomeViewModel.kt
+++ src/main/java/com/github/app/ui/main/home/HomeViewModel.kt
@@ -1,6 +1,6 @@
 package com.github.app.ui.main.home
 
-import android.util.Log
+
 import androidx.lifecycle.LiveData
 import androidx.paging.LivePagedListBuilder
 import androidx.paging.PagedList
@@ -8,11 +8,9 @@ import com.github.app.domain.entity.SearchRepo
 import com.github.app.domain.usecase.RepoListDataUseCase
 import com.github.app.reduce.Action
 import com.github.app.reduce.Change
-import com.github.app.reduce.Reducer
 import com.github.app.reduce.State
 import com.github.app.ui.base.BaseViewModel
 import com.github.app.util.Keys
-import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.ofType
 import io.reactivex.rxkotlin.plusAssign
 import io.reactivex.schedulers.Schedulers
@@ -20,42 +18,33 @@ import timber.log.Timber
 import javax.inject.Inject
 
 
-
-class HomeViewModel @Inject constructor(val repoListDataUseCase: RepoListDataUseCase) :
-    BaseViewModel<Action, State>() {
+class HomeViewModel @Inject constructor(private val repoListDataUseCase: RepoListDataUseCase) :
+        BaseViewModel<Action, State>() {
 
     var liveDataSearchRepoList: LiveData<PagedList<SearchRepo>>
-    private val compositeDisposable: CompositeDisposable = CompositeDisposable();
     private val perPage: Int = Keys.PER_PAGE
 
-
-
     init {
-        repoListDataUseCase.initSearchRepoUsacase(compositeDisposable)
+        repoListDataUseCase.initSearchRepoUsacase(disposables)
         val config = PagedList.Config.Builder()
-            .setPageSize(perPage)
-            .setInitialLoadSizeHint(perPage * 2)
-            .setEnablePlaceholders(false)
-            .build()
+                .setPageSize(perPage)
+                .setInitialLoadSizeHint(perPage * 2)
+                .setEnablePlaceholders(false)
+                .build()
         liveDataSearchRepoList = LivePagedListBuilder(repoListDataUseCase, config).build()
 
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        compositeDisposable.dispose()
     }
 
     override fun bind() {
         val userDetailRequest = actions.ofType<Action.ActionUserDetail>().switchMap {
             repoListDataUseCase.userDetail(it.ownerName).subscribeOn(Schedulers.io())
-                .map<Change> { Change.Loaded(it) }
-                .onErrorReturn { Change.LoadError(it) }
-                .startWith(Change.Loading)
+                    .map<Change> { Change.Loaded(it) }
+                    .onErrorReturn { Change.LoadError(it) }
+                    .startWith(Change.Loading)
         }
 
         disposables += userDetailRequest.scan(initialState, reducer)
-            .distinctUntilChanged()
-            .subscribe(state::postValue, Timber::e)
+                .distinctUntilChanged()
+                .subscribe(state::postValue, Timber::e)
     }
 }
